fe365dc10b786d80a6c00d345b2af38a6dab31d5
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2020-08-20 19:11:49 -0700

    Initial Commit

---
+++ src/com/Mathematician/DynamicInventories/DynamicInventoriesMain.java
@@ -0,0 +1,16 @@
+package com.Mathematician.DynamicInventories;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DynamicInventoriesMain extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
