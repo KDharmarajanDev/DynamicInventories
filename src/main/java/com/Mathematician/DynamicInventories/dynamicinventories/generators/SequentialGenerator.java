b326946f2c39828b62be4f62c7da2e9649626bea
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2020-08-24 16:07:49 -0700

    FUNCTION: Added duration information to each ItemState
    This commit adds duration to ItemStates. Duration is responsible for dictating how long a certain ItemState is displayed for. -1 is used to designate infinite amounts of time. The duration is a long defined in terms of milliseconds.

--- src/main/java/com/Mathematician/DynamicInventories/dynamicinventories/generators/SequentialGenerator.java
+++ src/main/java/com/Mathematician/DynamicInventories/dynamicinventories/generators/SequentialGenerator.java
@@ -1,5 +1,12 @@
 package com.Mathematician.DynamicInventories.dynamicinventories.generators;
 
+import com.Mathematician.DynamicInventories.dynamicinventories.itemstates.ItemState;
+
+import java.util.ArrayList;
+
 public class SequentialGenerator {
+
+    private ArrayList<ItemState> itemStates;
+    private int slotNumber;
 
 }
--- src/main/java/com/Mathematician/DynamicInventories/dynamicinventories/itemstates/ItemState.java
+++ src/main/java/com/Mathematician/DynamicInventories/dynamicinventories/itemstates/ItemState.java
@@ -10,11 +10,18 @@ public class ItemState {
     private ItemStack item;
 
     /**
+     * duration is a long that represents the length of time in milliseconds that this ItemState will be shown for. -1 represents an infinite amount of time.
+     */
+    private long duration;
+
+    /**
      * This is the only constructor for the ItemState.
      * @param item - an ItemStack that will be shown in the DynamicInventory when this state is displayed
+     * @param duration - a long that represents the length of time in milliseconds that this ItemState will be shown for. -1 represents an infinite amount of time.
      */
-    public ItemState(ItemStack item){
+    public ItemState(ItemStack item, long duration){
         this.item = item;
+        this.duration = duration;
     }
 
     /**
@@ -31,5 +38,21 @@ public class ItemState {
      */
     public void setItem(ItemStack item){
         this.item = item;
+    }
+
+    /**
+     * This is a getter that returns a long (duration).
+     * @return duration - a long that represents the length of time in milliseconds that this ItemState will be shown for. -1 represents an infinite amount of time.
+     */
+    public long getDuration(){
+        return duration;
+    }
+
+    /**
+     * This is a setter that sets duration (long).
+     * @param duration - a long that represents the length of time in milliseconds that this ItemState will be shown for. -1 represents an infinite amount of time.
+     */
+    public void setDuration(long duration){
+        this.duration = duration;
     }
 }
